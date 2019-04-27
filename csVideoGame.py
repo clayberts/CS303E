6df0b54eb3352ad8b87a4d6db3fddbeb32b42403
clayberts <49999989+clayberts@users.noreply.github.com>
2019-04-27 18:01:37 -0500

    Update csVideoGame.py

--- csVideoGame.py
+++ csVideoGame.py
@@ -3,16 +3,31 @@ import random
 """explaination of class"""
 
 def titleScreen():
+# print() statement 2 points
 	print("Welcome to Our Game!")
-	#print() statement 2 points
-	player = str(input("Please Enter a Username to Begin: ")
+	player = str(input("Please Enter a Username to Begin: "))
 	return player
 
-def gameplay():
-	player = titleScreen()
-	print(f"{player}, you wake up in an empty rooom aside from a door on the far wall and a desk with a note ontop of it")
-				 
-def main():
-	print("I hope we get a 100")
+def tutorialNote():
+# This function reads a text file that has the instructions for the game on it.
+# 2 Style of Comments (single and multiline) 
+# File Reading 15 points
+	with open("tutorial.txt", "r") as inFile:
 
-main()
+def gameplayLevelOne():
+# Formatting for strings 5 points ??
+# If statement 5 points
+
+	player = titleScreen()
+	skip = '\n'
+	print(f"{player}, you wake up in an empty room {skip}aside from an ajar door on the far wall {skip}and a desk with a note ontop of it.")
+	choice1 = input("Enter 'note' to read the note; Enter 'door' to cross through the doorway: ")
+	if choice1.lower() == "door":
+		print("You cross the room and pass through the frame of the door. Your journey begins...")
+	elif choice1.lower() == "note"
+		print("You pick the note up and examine it. The note looks to be covered in instructions")
+#		tutorialNote()
+		choice2 = input("Enter 'door' to cross through the doorway: ")
+		if choice2.lower == "door":
+			print("You cross the room and pass through the frame of the door. Your journey begins...")
+
