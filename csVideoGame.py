562942aefa2705cbe82524f70c6b9726e80ad145
clayberts <49999989+clayberts@users.noreply.github.com>
2019-04-27 17:48:41 -0500

    Update csVideoGame.py

--- csVideoGame.py
+++ csVideoGame.py
@@ -1,3 +1,17 @@
+import random
+#Class
+"""explaination of class"""
+
+def titleScreen():
+	print("Welcome to Our Game!")
+	#print() statement 2 points
+	player = str(input("Please Enter a Username to Begin: ")
+	return player
+
+def gameplay():
+	player = titleScreen()
+	print(f"{player}, you wake up in an empty rooom aside from a door on the far wall and a desk with a note ontop of it")
+				 
 def main():
 	print("I hope we get a 100")
 
