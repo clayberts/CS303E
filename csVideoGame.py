885d6c8f58dc64011bfe2d9e18ea588e4ff5bf01
siroky757 <50060393+siroky757@users.noreply.github.com>
2019-04-28 19:36:12 -0500

    Update csVideoGame.py

--- csVideoGame.py
+++ csVideoGame.py
@@ -31,3 +31,34 @@ def gameplayLevelOne():
 		if choice2.lower == "door":
 			print("You cross the room and pass through the frame of the door. Your journey begins...")
 
+def rollingDice():
+#This function uses the random number generator in order to have the computer roll two dice.
+    diceOne = random.randint(1,6)
+    diceTwo = random.randint(1,6)
+    return diceOne,diceTwo
+
+def comparingRolls(diceOne,diceTwo,dictionaryForRolls,health=100):
+#This function uses the recursive function definition and Recursive Function Call. It also uses -= 
+#This recursive function in the game helps determine what happens to the player’s health when some obstacle or object causes her/him to get stuck/injured. 
+    if diceTwo > diceOne:
+        return dictionaryForRolls['Win']
+    elif diceTwo < diceOne:
+        return dictionaryForRolls['Lose']
+    else:
+        health -= 5   # -= used to subtract the player’s health percentage by five each time he/she is stuck/injured.
+        print(dictionaryForRolls['Tie'])
+        print(f"Health level: {health}%")
+        diceOne,diceTwo = rollingDice()
+        return comparingRolls(diceOne,diceTwo,dictionaryForRolls,health)
+
+def main():
+#This function is where we will execute our entire game code.
+	
+    #line 59 below uses a dictionary. It is used as a parameter in the comparingRolls recursive function.
+    #lines 59-61 will be right next to each other in the finished game code as shown below. However right now these three lines in main() are just testing to see if the recursive function,dictionary, and print statement work together.
+    dictionaryForRolls = {'Tie':'You are stuck! Losing health fast!','Win':'You are safe! Continue forward','Lose':'Stuck for too long\nHealth level: 0%...Mission incomplete'}
+    diceOne,diceTwo = rollingDice()
+    print(comparingRolls(diceOne,diceTwo,dictionaryForRolls,health=100))
+
+	
+main()
